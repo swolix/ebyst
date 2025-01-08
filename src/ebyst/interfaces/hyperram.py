9ca722ad1667dd089a8a4ec92e9071dfe1e34943
Sijmen Woutersen <sijmen.woutersen@technolution.nl>
2025-01-08 09:58:03 +0100

    hyperram update

--- src/ebyst/interfaces/hyperram.py
+++ src/ebyst/interfaces/hyperram.py
@@ -22,97 +22,126 @@ from bitarray import bitarray
 from ..device import Pin, DiffPin, PinGroup
 
 class HyperRAM:
-    def __init__(self, ctl, CK: DiffPin, CSn: Pin, RWDS: Pin, DQ: PinGroup):
+    def __init__(self, ctl, CK: DiffPin, RESETn: Pin, CSn: Pin, RWDS: Pin, DQ: PinGroup):
         self.ctl = ctl
         self.CK = CK
+        self.RESETn = RESETn
         self.CSn = CSn
         self.RWDS = RWDS
         self.DQ = DQ
 
     async def init(self):
         self.CK.output_enable(True)
+        self.RESETn.output_enable(True)
         self.CSn.output_enable(True)
         self.RWDS.output_enable(False)
         self.DQ.output_enable(False)
         self.CK.set_value(0)
+        self.RESETn.set_value(0)
         self.CSn.set_value(1)
+        await self.ctl.cycle()
+        self.RESETn.set_value(1)
         await self.ctl.cycle()
 
     async def read(self, address):
-        self.RWDS.output_enable(True)
-        self.DQ.output_enable(True)
-        self.CSn.set_value(0)
-        self.RWDS.set_value(1)
+        try:
+            self.RWDS.output_enable(True)
+            self.DQ.output_enable(True)
+            self.CSn.set_value(0)
+            self.RWDS.set_value(1)
 
-        ca = [0x80 | ((address >> 27) & 0x1f),
-              (address >> 19) & 0xff,
-              (address >> 11) & 0xff,
-              (address >>  3) & 0xff,
-              0,
-              address & 0x07]
+            ca = [0x80 | ((address >> 27) & 0x1f),
+                (address >> 19) & 0xff,
+                (address >> 11) & 0xff,
+                (address >>  3) & 0xff,
+                0,
+                address & 0x07]
 
-        for i in range(3):
-            self.DQ = ca[2*i+0]
+            for i in range(3):
+                self.DQ.set_value(ca[2*i+0])
+                await self.ctl.cycle()
+                self.CK.set_value(1)
+                await self.ctl.cycle()
+                self.DQ.set_value(ca[2*i+1])
+                await self.ctl.cycle()
+                self.CK.set_value(0)
+                await self.ctl.cycle()
+
+            self.RWDS.output_enable(False)
+            self.DQ.output_enable(False)
+
+            r = []
+            rwds_d = rwds_dd = 0
+            for i in range(20*4):
+                self.CK.set_value(1 if i & 2 else 0)
+                await self.ctl.cycle()
+
+                if rwds_dd != rwds_d: r.append(self.DQ.get_value())
+                rwds_dd = rwds_d
+                rwds_d = self.RWDS.get_value()
+
+                if len(r) == 4: break
+
+            if len(r) != 4:
+                raise Exception("HyperRAM not responding")
+        finally:
+            self.CSn.set_value(1)
             await self.ctl.cycle()
-            self.CK.set_value(1)
-            await self.ctl.cycle()
-            self.DQ = ca[2*i+1]
-            await self.ctl.cycle()
+            self.RWDS.output_enable(False)
+            self.DQ.output_enable(False)
             self.CK.set_value(0)
             await self.ctl.cycle()
 
-        self.RWDS.output_enable(False)
-        self.DQ.output_enable(False)
-
-        for i in range(100):
-            self.CK.set_value(1)
-            await self.ctl.cycle()
-            await self.ctl.cycle()
-            self.CK.set_value(0)
-            await self.ctl.cycle()
-            await self.ctl.cycle()
-
+        return r
 
     async def write(self, address, data):
-        self.RWDS.output_enable(True)
-        self.DQ.output_enable(True)
-        self.CSn.set_value(0)
-        self.RWDS.set_value(1)
+        try:
+            self.RWDS.output_enable(True)
+            self.DQ.output_enable(True)
+            self.CSn.set_value(0)
+            self.RWDS.set_value(1)
 
-        ca = [0x80 | ((address >> 27) & 0x1f),
-              (address >> 19) & 0xff,
-              (address >> 11) & 0xff,
-              (address >>  3) & 0xff,
-              0,
-              address & 0x07]
+            ca = [0x00 | ((address >> 27) & 0x1f),
+                (address >> 19) & 0xff,
+                (address >> 11) & 0xff,
+                (address >>  3) & 0xff,
+                0,
+                address & 0x07]
 
-        for i in range(3):
-            self.DQ = ca[2*i+0]
-            await self.ctl.cycle()
-            self.CK.set_value(1)
-            await self.ctl.cycle()
-            self.DQ = ca[2*i+1]
-            await self.ctl.cycle()
-            self.CK.set_value(0)
-            await self.ctl.cycle()
+            for i in range(3):
+                self.DQ.set_value(ca[2*i+0])
+                await self.ctl.cycle()
+                self.CK.set_value(1)
+                await self.ctl.cycle()
+                self.DQ.set_value(ca[2*i+1])
+                await self.ctl.cycle()
+                self.CK.set_value(0)
+                await self.ctl.cycle()
 
-        for i in range(3+4):
-            await self.ctl.cycle()
-            self.CK.set_value(1)
-            await self.ctl.cycle()
-            await self.ctl.cycle()
-            self.CK.set_value(0)
-            await self.ctl.cycle()
+            self.RWDS.set_value(0)
 
-        for i in range(2):
-            self.DQ = (data >> (i * 8)) & 0xFF
-            await self.ctl.cycle()
-            self.CK.set_value(1)
-            await self.ctl.cycle()
-            self.DQ = (data >> ((i + 1) * 8)) & 0xFF
-            await self.ctl.cycle()
-            self.CK.set_value(0)
-            await self.ctl.cycle()
+            for i in range(2*7-1):
+                await self.ctl.cycle()
+                self.CK.set_value(1)
+                await self.ctl.cycle()
+                await self.ctl.cycle()
+                self.CK.set_value(0)
+                await self.ctl.cycle()
 
-        self.RWDS.output_enable(False)
-        self.DQ.output_enable(False)
+            self.RWDS.set_value(1)
+            for i in range(len(data)//2):
+                self.DQ.set_value(data[2*i])
+                await self.ctl.cycle()
+                self.CK.set_value(1)
+                await self.ctl.cycle()
+                self.DQ.set_value(data[2*i+1])
+                await self.ctl.cycle()
+                self.CK.set_value(0)
+                await self.ctl.cycle()
+
+        finally:
+            self.CSn.set_value(1)
+            await self.ctl.cycle()
+            self.RWDS.output_enable(False)
+            self.DQ.output_enable(False)
+            await self.ctl.cycle()
