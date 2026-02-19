bc4245c1afbd4cdb72b3458ffe008ffbe512ee3c
Sijmen Woutersen <sijmen.woutersen@technolution.nl>
2026-02-19 11:13:54 +0100

    add missing import

--- src/ebyst/interfaces/__init__.py
+++ src/ebyst/interfaces/__init__.py
@@ -1,7 +1,7 @@
 from .i2c import I2C
 from .spi import SPI
 from .mdio import MDIO
-from .flash import MT25Q
+from .flash import MT25Q, W25Q
 from .ddr3 import DDR3
 from .ddr4 import DDR4
 from .hyperram import HyperRAM
